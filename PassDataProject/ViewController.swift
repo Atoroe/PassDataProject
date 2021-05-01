ba5b8dcfc45396aed17469872d768922ae5c5fda
Artiom P <apoluyanovich@gmail.com>
2021-05-01 13:01:22 +0300

    Initial Commit

---
+++ PassDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Artiom Poluyanovich on 1.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
